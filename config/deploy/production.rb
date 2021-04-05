665365185ae715fe3e53bbd3b27601bab27d0bf9
stoicDavid <stoicdavid@gmail.com>
2021-04-04 23:32:24 -0500

    config puma 3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,8 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-ask(:password, nil, echo: false)
-server '165.232.136.224', user: 'deploy', roles: %w{app db web}, password: fetch(:password)
+server '165.232.136.224', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
--- config/puma/production.rb
+++ config/puma/production.rb
@@ -22,8 +22,11 @@ pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
+preload_app!
+
 on_worker_boot do
   require "active_record"
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
+
