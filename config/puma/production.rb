e67b8fabd80a4ccd6dca0c03d21117bf756d3b73
stoicDavid <stoicdavid@gmail.com>
2021-07-30 00:54:45 -0500

    puma4

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -18,8 +18,8 @@ bind "unix://#{shared_dir}/sockets/puma.sock"
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{shared_dir}/tmp/pids/puma.pid"
+state_path "#{shared_dir}/tmp/pids/puma.state"
 activate_control_app
 
 preload_app!
